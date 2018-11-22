44f5b0b176f51689b1bb5a14b614ae7b4171d675
Jonathan Walters <jonfwalters@gmail.com>
2018-11-22 21:27:31 +0000

    answers routing completed and working

--- run.py
+++ run.py
@@ -1,43 +1,42 @@
 import os
 import json
 import random
-from flask import Flask, render_template, request, flash, redirect, url_for, session
+from flask import Flask, render_template, session, request, flash, redirect, url_for
 
 app = Flask(__name__)
 app.secret_key = "secret"
 app.secret_key = os.urandom(24)
 
-"""Read userfile for registered users"""
 def users():
     with open("data/users.txt", "r") as user_data:
         registered_users = user_data.read().splitlines()
         return registered_users
-        
-"""Start game variables"""
-def start_game():
-    session['question_number'] = 1
-    session['players'] = generate_random_player()
-    session['user_score'] = 0
-    return session['question_number'], session['players'], session['user_score']
     
-"""Generate Random Player Image"""
 def generate_random_player():
     with open("data/players.json", "r") as players_data:
         players_list = json.load(players_data)["players"]
         random.shuffle(players_list)
         return players_list
         
-"""Scores"""
+def start_game():
+    session['question_number'] = 1
+    session['players'] = generate_random_player()
+    session['score'] = 0
+    return session['question_number'], session['players'], session['score']
+        
 def user_scores():
     with open("data/scores.json", "r") as score_data:
         data = json.load(score_data)
         return data
-        
-"""Routing"""
+   
+"""Routes"""
 
 @app.route('/')
 def index():
-    return render_template("index.html")
+    if session:
+        return redirect(url_for("play"))
+    else:
+        return render_template("index.html")
     
 @app.route('/login', methods = ["GET", "POST"])
 def login():
@@ -51,20 +50,21 @@ def login():
                 session['user'] = returning_user
                 return redirect(url_for("play"))
             else:
-                flash('The username "{}" is not recognized. Please enter a registered username '.format(request.form["username"]))
+                flash('The username "{}" is not recognized. Please register '.format(request.form["username"]))
         return render_template("login.html")
 
 @app.route('/logout')
 def logout():
-        session.clear
+        session.clear()
         return redirect(url_for("index"))
     
-app.route('/register')
+"""app.route('/register')
 def register():
     if session:
         return redirect(url_for("play"))
     else:
         return render_template("register.html")
+"""
 
 @app.route('/register', methods = ["GET", "POST"])
 def register_user():
@@ -89,18 +89,30 @@ def play():
     if session:
         users()
         start_game()
-        registered_users = users()
         question_number = session['question_number']
-        data = session['players']
-        score = session['user_score']
-        return render_template("play.html", question_number=question_number, players=data, user_score=score)
+        players = session['players']
+        score = session['score']
+        return render_template("play.html", question_number=question_number, players=players, score=score)
     else:
         return redirect(url_for("index"))
-    
-    
-@app.route('/leaderboard') 
-def leaderboard():
-    return render_template("leaderboard.html")
+ 
+@app.route('/play', methods = ["POST"])
+def answers():
+    if session:
+        players = session['players']
+        if request.method == "POST":
+            session['question_number'] +=1
+            session['guess'] = request.form.get("user_answer").lower()
+            session['correct_answer'] = request.form.get("answer")
+            if session['guess'] == session['correct_answer']:
+                session['score'] +=1
+            return render_template("play.html", players=players, question_number=session['question_number'], guess=session['guess'], correct_answer=session['correct_answer'], score=session['score'])
+    else:
+        return redirect(url_for("index"))
+      
+@app.route('/scoreboard') 
+def scoreboard():
+    return render_template("scoreboard.html")
 
 @app.route('/contact')
 def contact():
@@ -110,4 +122,3 @@ if __name__=='__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
             debug=True)
-            
